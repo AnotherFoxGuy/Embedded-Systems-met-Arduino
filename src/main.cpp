c15edaea719135dc8b5cd6c0396a16a5e81c72a5
Edgar <edgar@anotherfoxguy.com>
2019-05-09 13:49:00 +0200

    :sparkles: Week 1&2

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,90 @@
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
 #include "Arduino.h"
+#include <WString.h>
 
-// Set LED_BUILTIN if it is not defined by Arduino framework
-// #define LED_BUILTIN 13
+#define OPDRACHT 11
+#define SEPERATOR Serial.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-int st_pins [3] = { 13, 12, 11 };
+#if OPDRACHT == 8
+int pins[] = {8,9,10,11,12,13};
+#endif
+
 
 void setup()
 {
-    // initialize LED digital pin as an output.
-    pinMode(13, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(11, OUTPUT);
+    Serial.begin(9600);
+
+#if OPDRACHT == TEST
+   
+ 
+    SEPERATOR
+    String test = "Hello";
+    test += " test";
+    test += " sdsdas";
+    test += " sdffgd";
+    Serial.println(test);
+
+    SEPERATOR
+
+#endif
+#if OPDRACHT == 7
+    Serial.begin(9600);
+ 
+    SEPERATOR
+    Serial.println("A");
+    Serial.println(M_PI * sq(4));
+    SEPERATOR
+    
+    SEPERATOR
+    Serial.println("B");
+    for(int i = 1; i <= 10; i++){
+        Serial.println(M_PI * sq(i));
+    }
+    SEPERATOR
+
+    SEPERATOR
+    Serial.println("C");
+    Serial.println(sqrt(sq(6 - 3) + sq(8 - 4) + sq(4 - 2)));
+    SEPERATOR
+#endif
+
+#if OPDRACHT == 8
+    for (int i = 0; i < (sizeof(pins) / sizeof(pins[0])); i++){
+      pinMode(pins[i], OUTPUT);
+    }
+#endif
+
+#if OPDRACHT == 10
+     pinMode(11, OUTPUT);
+#endif
 }
 
 void loop()
 {
-    digitalWrite(11, HIGH);
-    digitalWrite(13, LOW);
-    delay(2000);
-
-    digitalWrite(11, LOW);  
-    digitalWrite(12, HIGH);
+#if OPDRACHT == 8
+    for (int i = 0; i < (sizeof(pins) / sizeof(pins[0])); i++){
+        digitalWrite(pins[i], HIGH);
+        delay(100);
+        digitalWrite(pins[i], LOW);
+        delay(100);
+    }
+    PORTB = 0x3F;
     delay(1000);
-
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
-    delay(2000);
-
-    digitalWrite(12, HIGH);
+    PORTB = 0x0;
     delay(1000);
+#endif
 
-    digitalWrite(13, LOW);
-    digitalWrite(12, LOW);
-    digitalWrite(11, HIGH);
-    delay(2000);
+
+#if OPDRACHT == 10
+    for (int i = 0; i < 255; i++){
+        analogWrite(11, i);
+        Serial.println(i);
+        delay(10);
+    }
+    delay(1000);
+#endif
+
+#if OPDRACHT == 11
+    Serial.println(analogRead(A0));          // debug value
+    delay(100);
+#endif
 }
